2343295aa0a06a8ad1d0bc83a68b6effa99229ac
Zaqi <zaqifathis@gmail.com>
2021-07-29 08:46:18 +0800

    sitecloc

--- src/script.js
+++ src/script.js
@@ -1,12 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
 import { BufferAttribute, Color, Float32BufferAttribute } from 'three'
 import cities from 'cities.json'
 
-// Debug
-// const gui = new dat.GUI()
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -14,17 +12,8 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Sphere
-// const geometry = new THREE.SphereBufferGeometry(1, 30, 30)
-// const material = new THREE.PointsMaterial({
-//     size: 0.008
-// })
-// const sphere = new THREE.Points(geometry, material)
-// scene.add(sphere)
-
 /*
-Get Coordinates 
-from lat, lng > vector3
+Globe Particle Coordinates
 */
 const latitudes = []
 const longitudes = []
@@ -33,8 +22,6 @@ for (let i = 0; i < cities.length; i++) {
     latitudes.push(parseFloat(cities[i].lat))
     longitudes.push(parseFloat(cities[i].lng))
 }
-
-console.log(latitudes.length)
 
 var getCoordinatesFromLatLng = function (latitude, longitude, radiusEarth) {
     let latitude_rad = latitude * Math.PI / 180;
@@ -50,18 +37,19 @@ var getCoordinatesFromLatLng = function (latitude, longitude, radiusEarth) {
 const ptCoor = new THREE.BufferGeometry()
 
 const geoLengths = latitudes.length
+const globeRad = 1
 
 const geoCoord = []
 for (let i = 0; i < geoLengths; i++) {
-    geoCoord[i] = getCoordinatesFromLatLng(latitudes[i], longitudes[i], 1)
+    geoCoord[i] = getCoordinatesFromLatLng(latitudes[i], longitudes[i], globeRad)
 }
 
 const position = []
 
-for( let i = 0; i < geoLengths; i++){
+for (let i = 0; i < geoLengths; i++) {
     position.push(
-        geoCoord[i].x ,
-        geoCoord[i].y ,
+        geoCoord[i].x,
+        geoCoord[i].y,
         geoCoord[i].z
     )
 }
@@ -76,10 +64,56 @@ const ptMat = new THREE.PointsMaterial({
 const mesh = new THREE.Points(ptCoor, ptMat)
 scene.add(mesh)
 
+/*
+Sites Coordinate
+*/
+const sites = require('./sites.json')
+
+const sitesLat = []
+const sitesLng = []
+
+for (let i = 0; i < sites.length; i++) {
+    sitesLat.push(parseFloat(sites[i].latlng.lat))
+    sitesLng.push(parseFloat(sites[i].latlng.lng))
+}
+
+// const sitesPts = new THREE.BufferGeometry()
+
+const sitesCoord = []
+const sitesPos = []
+
+for (let i = 0; i < sites.length; i++) {
+    sitesCoord[i] = getCoordinatesFromLatLng(sitesLat[i], sitesLng[i], globeRad)
+}
+
+for (let i = 0; i < sitesLat.length; i++) {
+    sitesPos.push(
+        sitesCoord[i].x,
+        sitesCoord[i].y,
+        sitesCoord[i].z
+    )
+}
+
+// sitesPts.setAttribute('position', new Float32BufferAttribute(sitesPos, 3))
+
+// const sitesMat = new THREE.PointsMaterial({
+//     size: 0.02,
+//     color: 'white'
+// })
+
+// const mesh2 = new THREE.Points(sitesPts, sitesMat)
+// scene.add(mesh2)
+
+const sitesPts = new THREE.PointLight(0xff0000, 100, 100)
+const posArr = new Float32Array(sitesPos, 3)
+sitesPts.position.set(posArr)
+scene.add(sitesPts)
+
+console.log(ptCoor)
 
 //Particles
 const particlesGeo = new THREE.BufferGeometry()
-const particlesCount = 6000
+const particlesCount = 1000
 
 const posArray = new Float32Array(particlesCount * 3)
 
@@ -90,16 +124,12 @@ for (let i = 0; i < particlesCount; i++) {
 
 particlesGeo.setAttribute('position', new THREE.BufferAttribute(posArray, 3))
 
-console.log(particlesGeo.attributes.position.array.length)
-console.log(particlesGeo.attributes)
-
 const particleMat = new THREE.PointsMaterial({
-    size: 0.01
+    size: 0.0001
 })
 
 const particlesMesh = new THREE.Points(particlesGeo, particleMat)
 scene.add(particlesMesh)
-
 
 
 // Update Sizes
@@ -162,12 +192,11 @@ const animate = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    // sphere.rotation.y = .1 * elapsedTime
     mesh.rotation.y = .1 * elapsedTime
+    sitesPts.rotation.y = .1 * elapsedTime
 
     particlesMesh.rotation.y = .1 * elapsedTime
 
-    // particlesMesh.rotation.z = .01 * elapsedTime
     particlesMesh.rotation.x = mouseY * (elapsedTime * 0.00009)
     particlesMesh.rotation.y = mouseX * (elapsedTime * 0.00009)
 
